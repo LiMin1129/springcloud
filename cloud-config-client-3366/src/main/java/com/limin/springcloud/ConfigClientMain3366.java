cd5213668ada1a99742bab4ba206141eb826839a
limin <75207269@.com>
2022-01-10 16:45:45 +0800

    First commit 2022/1/9

---
+++ cloud-config-client-3366/src/main/java/com/limin/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.limin.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author LiMin
+ * @date 2022/1/10-16:09
+ * @DESCRIPTION
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
